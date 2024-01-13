c60b5006f2e8d85f7f3b9d9cc4efca573cb30cd0
Pathe SENE <pathe.sene@gmail.com>
2024-01-13 13:11:33 +0000

    chore: add tests for board search

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  it 'doest not allow negative number of mines'
+  describe ".search" do
+    it 'raises error when search criteria is unknown' do
+      search_params = { by_name: 'test' }
+      expect { Board.search(search_params) }.to raise_error(BoardSearchCriteriaNotFound)
+    end
+  end
 end
